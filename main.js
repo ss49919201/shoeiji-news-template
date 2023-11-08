72175981e421b934e130007f4d4f72cbdf272660
ss49919201 <sakashin10291029@gmail.com>
2023-11-08 22:40:56 +0900

    read template from file

--- main.js
+++ main.js
@@ -1,48 +1,36 @@
 import * as fs from 'node:fs';
 
-function format(content1, content2, content3) {
-  return `<!doctype html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8" />
-    <title>正栄寺新聞</title>
-	<link rel="stylesheet" href="styles.css" />
-  </head>
-  <body>
-    <div class="container header">
-      title
-    </div>
-    <div class="container content">
-      ${content1}
-    </div>
-    <div class="container content">
-      ${content2}
-    </div>
-    <div class="container content">
-      ${content3}
-    </div>
-  </body>
-</html>`
+function formatHTML(templateHTML, content1, content2, content3) {
+  return templateHTML.
+    replace('${content1}', content1).
+    replace('${content2}', content2).
+    replace('${content3}', content3);
 }
 
 function validate(s) {
   return s.length < 300
 }
 
+function validateContents(content1, content2, content3) {
+  [content1, content2, content3].forEach((v) => {
+    if (!validate(v)) {
+      throw new Error("文字列が不正です")
+    };
+  });
+}
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat('ja-JP', {
+    dateStyle: 'medium',
+    timeStyle: 'medium'
+  }).format(date).replace(/[\/: ]/g, '');
+}
+
 const contents = ["", "", ""];
-
-contents.forEach((v) => {
-  console.log(v);
-  if (!validate(v)) {
-    throw new Error("文字列が不正です")
-  };
-});
-
-// console.log(format(...contents));
+const templateHTML = fs.readFileSync('./template.html', {encoding: 'utf8'});
+validateContents(...contents);
 const date = new Date();
-const formattedDate = new Intl.DateTimeFormat('ja-JP', {
-  dateStyle: 'medium',
-  timeStyle: 'medium'
-}).format(date).replace(/[\/: ]/g, '');
+const formattedDate = formatDate(date)
+const formattedHTML = formatHTML(templateHTML, ...contents);
 fs.mkdirSync(`./${formattedDate}`);
-fs.writeFileSync(`./${formattedDate}/index.html`, format(...contents));
+fs.writeFileSync(`./${formattedDate}/index.html`, formattedHTML);
